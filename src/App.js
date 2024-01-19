700774589202a1fa0cf17519c8dc58589b1c9035
YildizGokhan <gygokhanyildiz1@gmail.com>
2024-01-19 15:47:43 +0300

    loginform completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+
 import store from "./app/store";
 import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux"
+
 
 function App() {
   return (
     
       <Provider store={store} >
+      
         <AppRouter />
       </Provider>
     
